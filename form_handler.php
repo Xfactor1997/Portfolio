5a85ed2811c35869051675ea5661d8828c622b23
Xfactor1997 <122984270+Xfactor1997@users.noreply.github.com>
2023-06-18 02:10:46 +0300

    Add files via upload

--- form_handler.php
+++ form_handler.php
@@ -1,5 +1,5 @@
 <?php
-header('Access-Control-Allow-Origin:/index.html');
+header('Access-Control-Allow-Origin:');
 header('Access-Control-Allow-Methods: GET, POST');
 header('Access-Control-Allow-Headers: Content-Type');
 $name = $_POST["name"];
