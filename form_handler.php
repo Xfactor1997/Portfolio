781461ff39a8db57c55578e2162886e3cd5f80de
Xfactor1997 <122984270+Xfactor1997@users.noreply.github.com>
2023-06-19 03:35:52 +0300

    Add files via upload

--- form_handler.php
+++ form_handler.php
@@ -1,36 +1,39 @@
 <?php
-header('Access-Control-Allow-Origin:');
-header('Access-Control-Allow-Methods: GET, POST');
-header('Access-Control-Allow-Headers: Content-Type');
-$name = $_POST["name"];
-$email = $_POST["email"];
-$mobile = $_POST["mobile"];
-$subject = $_POST["subject"];
-$message = $_POST["message"];
+require_once('./index.php');
 
-$mailheader = "From:" . $name . "<" . $email . ">\r\n";
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-$recipient = "markgamal722@gmail.com";
-mail($recipient,$subject,$message,$mailheader)
-or die("Error!");
-echo "<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../CSS/style.css">
-    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-    <title>Mark Gamal</title>
-</head>
-<body>
-    <div class="container">
-        <h1>
-            Thank you for contacting me.
-            I will get back to you as soon as possible!
-        </h1>
-        <p class="back">Go back to the <a href="index.html">homepage</a>.</p>
-    </div>
-</body>
-</html>
-";
+require './vendor/phpmailer/phpmailer/src/Exception.php';
+require './vendor/phpmailer/phpmailer/src/PHPMailer.php';
+require './vendor/phpmailer/phpmailer/src/SMTP.php';
+
+if(isset($_POST["send"])) {
+    $mail = new PHPMailer(true);
+
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'markgamal722@gmail.com';
+    $mail->Password = 'zzzgdwpirlmdqdig';
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port = 465;
+
+    $mail->setFrom('markgamal722@gmail.com');
+
+    $mail->addAddress($_POST["email"]);
+
+    $mail->isHTML(true);
+    $mail->Subject = $_POST["subject"];
+    $mail->Body = $_POST["message"];
+
+    $mail->send();
+
+    echo
+        "
+    <script>
+    alert('Sent Successfully');
+    document.location.href = 'index.html';
+    </script>
+    ";
+}
--- index.php
+++ index.php
@@ -1,3 +1,24 @@
 <?php
 
+
+
+
+header('Access-Control-Allow-Origin:https://markgamal.netlify.app/');
+header('Access-Control-Allow-Methods: GET, POST');
+header('Access-Control-Allow-Headers: Content-Type');
+// $name = $_POST["name"];
+// $email = $_POST["email"];
+// $mobile = $_POST["mobile"];
+// $subject = $_POST["subject"];
+// $message = $_POST["message"];
+
+// $mailheader = "From:" . $name . "<" . $email . ">\r\n";
+
+// $recipient = "markgamal722@gmail.com";
+// mail($recipient,$subject,$message,$mailheader)
+// or die("Error!");
+// echo "Message Sent Successfully!";
+
+
+
 require_once('./form_handler.php');
